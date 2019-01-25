b3b968b8d053abb31007d2b6781c3dcd8328e187
hegeteus <45102227+hegeteus@users.noreply.github.com>
2019-01-25 22:30:17 +0200

    animointeja aseelle, heittohomma
    asd

---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+    public float force = 1;
+    public bool objectGrabbed;
+    private GameObject grabbedObject;
+    private bool objectInRange;
+    private GameObject grabbableObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(grabbableObject.name);
+
+       
+    }
+
+    //Täällä kehotetaan tekemään fysiikkajutut
+    private void LateUpdate()
+    {
+        if (Input.GetButtonDown("Fire2") && objectGrabbed == false && grabbableObject.GetComponent<ThrowableObject>().canBeThrown == true && objectInRange == true)
+        {
+            objectGrabbed = true;
+            grabbableObject.transform.SetParent(gameObject.transform);
+            grabbableObject.transform.rotation = gameObject.transform.rotation;
+            grabbedObject = grabbableObject.transform.gameObject;
+        }
+
+        else if (Input.GetButtonDown("Fire2") && objectGrabbed == true)
+        {
+            objectGrabbed = false;
+            grabbedObject.transform.SetParent(null);
+            grabbedObject.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+        }
+    }
+
+    private void OnTriggerStay(Collider obj)
+    {
+        grabbableObject = obj.gameObject;
+        objectInRange = true;
+
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        objectInRange = false;
+
+    }
+}
---
+++ Assets/Scripts/WeaponBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class WeaponBehavior : MonoBehaviour
+{
+    public List<string> animations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && AnimationsPlaying(0) == false)
+        {
+            gameObject.GetComponent<Animator>().Play(RandomWeaponAnimation(),0,0f);
+        }
+    }
+
+    public bool AnimationsPlaying(int layer)
+    {
+        foreach(string a in animations)
+        {
+            if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(layer).IsName(a))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public string RandomWeaponAnimation()
+    {
+        int tester = Random.Range(0, animations.Count);
+        print(tester);
+
+        return animations[tester];
+    }
+}
