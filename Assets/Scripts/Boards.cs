345b17dd3e4042d4a1a8f5317f81010eb6fbc741
OneNot <www.1n07@gmail.com>
2019-01-26 22:49:36 +0200

    xyrfj

--- Assets/Scripts/Boards.cs
+++ Assets/Scripts/Boards.cs
@@ -107,6 +107,7 @@ public class Boards : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             other.GetComponent<Patrol>().attackstate = true;
             print("triggered");
 =======
@@ -114,6 +115,10 @@ public class Boards : MonoBehaviour
 =======
             
 >>>>>>> 23e8da7d5adc7b28616e2cc7b88770d4f3fc978a
+=======
+            other.GetComponent<Patrol>().attackstate = true;
+            print("triggered");
+>>>>>>> parent of 6e7e398... vittu saatana
 =======
             other.GetComponent<Patrol>().attackstate = true;
             print("triggered");
